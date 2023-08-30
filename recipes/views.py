24544a66ec035af82a35b639a18c304116bb36d8
Rodrigo Rocha Meireles <rodrigomeireles.dev@gmail.com>
2023-08-30 15:03:58 -0300

    criação dos templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Hello, world')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-
-def contato(request):
-    return HttpResponse('contato')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Rodrigo'})

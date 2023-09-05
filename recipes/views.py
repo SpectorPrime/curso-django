47d80b9286910c8b648582c3feb3058705f4e589
Rodrigo Rocha Meireles <rodrigomeireles.dev@gmail.com>
2023-09-05 00:28:50 -0300

    Criação do read, onde eu explico o projeto

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
 
+# Essa view coleta request e retorna o html "home" do site #
 def home(request):
     return render(request, 'recipes/pages/home.html',
                   context={'name': 'Rodrigo'})
 
 
+# Essa view coleta o id de cada receita e retorna a página html
+# que destaca a receita
 def recipes(request, id):
     return render(request, 'recipes/pages/recipe-view.html',
                   context={'name': 'Rodrigo'})

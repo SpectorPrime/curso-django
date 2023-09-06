9c7a8f8d34896b09b46e704deeaf28171977e9bc
Rodrigo Rocha Meireles <rodrigomeireles.dev@gmail.com>
2023-09-05 21:42:55 -0300

    função teste para inserir receitas

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
+
+# Essa view coleta request e retorna o html "home" do site
+# No context, ele chama a função criada em factory.py faz um laço 
+# para gerar 6 receitas
 
 
-# Essa view coleta request e retorna o html "home" do site #
 def home(request):
     return render(request, 'recipes/pages/home.html',
-                  context={'name': 'Rodrigo'})
+                  context={'recipes': [make_recipe() for _ in range(9)]})
 
 
 # Essa view coleta o id de cada receita e retorna a página html
 # que destaca a receita
 def recipes(request, id):
     return render(request, 'recipes/pages/recipe-view.html',
-                  context={'name': 'Rodrigo'})
+                  context={'recipe': make_recipe()})

f1f6348e9e5f63120277a51d9ea2b676a704e710
Rodrigo Rocha Meireles <rodrigomeireles.dev@gmail.com>
2023-09-10 19:01:36 -0300

    Url e view category

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 # Essa view coleta request e retorna o html "home" do site
 # No context, ele chama a função criada em factory.py faz um laço 
@@ -7,8 +8,17 @@ from utils.recipes.factory import make_recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html',
-                  context={'recipes': [make_recipe() for _ in range(9)]})
+                  context={'recipes': recipes})
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True
+        ).order_by('-id')
+    return render(request, 'recipes/pages/category.html',
+                  context={'recipes': recipes})
 
 
 # Essa view coleta o id de cada receita e retorna a página html
